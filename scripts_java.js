bc804fb046300e48110911992344dc9c4ae8b993
Vitor Pereira <vitorpmh@gmail.com>
2022-10-11 18:02:52 -0300

    teste 2

--- scripts_java.js
+++ scripts_java.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
     $('#header2').load('https://www.uel.br/eventos/semat/header.html');
   });
+
+
+  $(document).ready(function () {
+    CheckForHighlight();
+});
+
+function CheckForHighlight() {
+    href = window.location.href;
+    values = href.split('?')[1] // Remove the url ".html?highlight=#try2"
+    highlight = values.split('=#')[1]; // divide em =# e pega o segundo parametro
+    $('#' + highlight).addClass('highlightedElem');
+    //highlightedElemclass has box shadow or border
+}
